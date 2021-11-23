422dde63d33003dbc87d01483ff59052e21fe846
rafaelwitter <rafawitt@hotmail.com>
2021-11-23 13:45:05 -0300

    Create admin page for Course and Student model

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from courses.models import Course
 
-# Register your models here.
+class Courses(admin.ModelAdmin):
+    list_display = ('id_course', 'name', 'description', 'duration')
+    list_display_links = ('id_course', 'name')
+    search_fields = ('name',)
+
+admin.site.register(Course, Courses)
--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from students.models import Student
 
-# Register your models here.
+class Students(admin.ModelAdmin):
+    list_display = ('id_student', 'name', 'nickname', 'phone')
+    list_display_links = ('id_student', 'nickname')
+    search_fields = ('nickname',)
+
+admin.site.register(Student, Students)
