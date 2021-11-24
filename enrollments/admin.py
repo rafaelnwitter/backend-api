af52be38b3d28be48244b001feff05c8347fde11
rafaelwitter <rafawitt@hotmail.com>
2021-11-24 13:49:16 -0300

    Add enrollment to admin page

--- enrollments/admin.py
+++ enrollments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from enrollments.models import Enrollment
 
-# Register your models here.
+class Enrollments(admin.ModelAdmin):
+    list_display = ('id', 'score', 'status')
+    list_display_links = ('id', 'score', 'status')
+    search_fields = ('status',)
+
+admin.site.register(Enrollment, Enrollments)
