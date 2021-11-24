990bbed1d3ee7b92f5fb057b25e21dd88c842467
rafaelwitter <rafawitt@hotmail.com>
2021-11-24 13:48:58 -0300

    Create stundet TDD

--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from students.models import Student
 
-# Create your tests here.
+class StudentTest(TestCase):
+    def setUp(self) -> None:
+        Student.objects.create(
+            name = 'Gustavo Frida',
+            nickname = 'guga.frida',
+            phone = '48996486598',
+            avatar = 'essaehminhafoto.jpg'
+        )
+
+    def test_stundet_create(self):
+        student_check = Student.objects.get(nickname='guga.frida')
+    
+    def test_student_delete(self):
+        student_checker = Student.objects.exclude(nickname='guga.frida')
