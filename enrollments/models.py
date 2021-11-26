7e594e9b28ddf0913b6a5e6c5b82f348204d6327
rafaelwitter <rafawitt@hotmail.com>
2021-11-26 19:13:18 -0300

    Update enrollment model

--- enrollments/models.py
+++ enrollments/models.py
@@ -5,14 +5,20 @@ from students.models import Student
 from courses.models import Course
 
 class Enrollment(models.Model):
-    id_student = models.ManyToManyField(Student)
-    id_course =  models.ManyToManyField(Course)
+    student = models.ForeignKey(Student, on_delete=models.SET_NULL, null=True)
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
     date_enroll = models.DateField()
     date_close = models.DateField()
     score = models.IntegerField()
     status_choice = (
-        ("Ap", "Aprovado"),
-        ("Rp", "Reprovado"),
-        ("Ad", "Andamento")
+        ("Aprovado", "Aprovado"),
+        ("Reprovado", "Reprovado"),
+        ("Andamento", "Andamento")
     )
-    status = models.CharField(max_length=2, choices=status_choice, blank=False, null=False, default="Ad")
+    status = models.CharField(max_length=10, choices=status_choice, blank=False, null=False, default="Ad")
+
+    def id_student(id):
+        id_student = id
+
+    def id_course(id):
+        id_course = id
