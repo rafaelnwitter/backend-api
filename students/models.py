efca57efe01e397956a75760bde3247fbdd6884a
rafaelwitter <rafawitt@hotmail.com>
2021-11-27 15:21:21 -0300

    Add unique fields to student model

--- students/models.py
+++ students/models.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 class Student(models.Model):
     id_student = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=20)
-    nickname = models.CharField(max_length=15)
+    nickname = models.CharField(max_length=15, unique=True)
     phone = models.CharField(max_length=12)
     avatar = models.CharField(max_length=255)
     created_at = models.DateField(auto_now_add=True)
